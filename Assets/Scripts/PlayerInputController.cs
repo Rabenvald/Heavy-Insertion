040f3be5b2e0e881ab7f9134d5a3a2df980816d6
mlz4243 <mlz4243@rit.edu>
2012-02-29 16:42:06 -0500

    changes made to allow for chat ingame. Changed TestLobby.cs to not show gui elements

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -131,41 +131,56 @@ public class PlayerInputController : InputController
 
     void Update()
     {
-        Throttle = Input.GetAxis("Vertical");
-        Yaw = Input.GetAxis("Horizontal");
-        Jump = Input.GetAxis("Jump");
-        PrimaryFire = Input.GetButton("Fire1");
-        SecondaryFire = Input.GetButton("Fire2");
-		Strafe = Input.GetAxis("Strafe");
-		
-		//Code for map and respawn
-        //mapCamera = GameObject.FindWithTag("MapCamera");
-		
-		if (Input.GetButton("Map"))
-		{
-            driving = false;
-			mapCamera.camera.enabled = true;
-			mainCamera.camera.enabled = false;
-            Screen.showCursor = false;
-			
-			/*RaycastHit hit;
-			if(Input.GetMouseButtonDown(0))
-			{	
-				if(Physics.Raycast(mapCamera.camera.ScreenPointToRay(Input.mousePosition), out hit))
-				{
-					Vector3 pos = new Vector3(hit.point.x, 2000, hit.point.z);
-					gameObject.GetComponent<Hovercraft>().respawn(pos);
-					Manager.Instance.sendSpawnData(pos);
-					Manager.Instance.Spawned = true;
-				}
-			}*/
-		}
-		else
-		{
-            driving = true;
-			mapCamera.camera.enabled = false;
-            mainCamera.camera.enabled = true;
-            Screen.showCursor = true;
-		}
+        enterJustPressed = false;
+        if (mainCamera.GetComponent<MainCameraScript>().typing == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                mainCamera.GetComponent<MainCameraScript>().typing = true;
+                enterJustPressed = true;
+            }
+
+            Throttle = Input.GetAxis("Vertical");
+            Yaw = Input.GetAxis("Horizontal");
+            Jump = Input.GetAxis("Jump");
+            PrimaryFire = Input.GetButton("Fire1");
+            SecondaryFire = Input.GetButton("Fire2");
+            Strafe = Input.GetAxis("Strafe");
+
+            //Code for map and respawn
+            //mapCamera = GameObject.FindWithTag("MapCamera");
+
+            if (Input.GetButton("Map"))
+            {
+                driving = false;
+                mapCamera.camera.enabled = true;
+                mainCamera.camera.enabled = false;
+                Screen.showCursor = false;
+
+                /*RaycastHit hit;
+                if(Input.GetMouseButtonDown(0))
+                {	
+                    if(Physics.Raycast(mapCamera.camera.ScreenPointToRay(Input.mousePosition), out hit))
+                    {
+                        Vector3 pos = new Vector3(hit.point.x, 2000, hit.point.z);
+                        gameObject.GetComponent<Hovercraft>().respawn(pos);
+                        Manager.Instance.sendSpawnData(pos);
+                        Manager.Instance.Spawned = true;
+                    }
+                }*/
+            }
+            else
+            {
+                driving = true;
+                mapCamera.camera.enabled = false;
+                mainCamera.camera.enabled = true;
+                Screen.showCursor = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && !enterJustPressed)
+        {
+            mainCamera.GetComponent<MainCameraScript>().sendMsg();
+        }
     }
 }
